355993b7c7b427efacc042e0c472d6dd8558ac50
mik284 <mikeykamau222@gmail.com>
2023-02-28 13:45:58 +0300

    added firebase config

---
+++ src/config/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAdDvzRCgveDyxYB81cR_X9XBlla-VbasI",
+  authDomain: "house-market-9b4de.firebaseapp.com",
+  projectId: "house-market-9b4de",
+  storageBucket: "house-market-9b4de.appspot.com",
+  messagingSenderId: "112523970746",
+  appId: "1:112523970746:web:033ee820e06023f8dc1a83"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
