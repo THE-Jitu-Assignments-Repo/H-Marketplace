1d80e7e4993edb3958c8dfc896afbd9f88545a96
mik284 <mikeykamau222@gmail.com>
2023-03-05 23:55:55 +0300

    added navbar footer

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,6 +3,13 @@ import React from 'react'
 function Navbar() {
     return (
         <footer className='navbar'>
+            <nav className="navbarBav">
+                <ul className="navbarListItems">
+                    <li className="navbarListItem">
+                        
+                    </li>
+                </ul>
+            </nav>
             
         </footer>
     )
