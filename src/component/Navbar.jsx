c9476cf8af2c8964644aa33347ab19c71ff5f3a0
mik284 <mikeykamau222@gmail.com>
2023-03-08 23:47:57 +0300

    modified navbar component

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import  OfferIcon  from '../assets/svg/localOfferIcon.svg'
-import  ExploreIcon  from '../assets/svg/exploreIcon.svg'
-import  PersonOutlineIcon from '../assets/svg/personOutlineIcon.svg'
+import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg'
+import  { ReactComponent as ExploreIcon}  from '../assets/svg/exploreIcon.svg'
+import  { ReactComponent as PersonOutlineIcon} from '../assets/svg/personOutlineIcon.svg'
 
 
 
