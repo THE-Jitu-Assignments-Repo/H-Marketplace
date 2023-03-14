20a9ef39f15fccd7504df52f2042c0cd632646c1
mik284 <mikeykamau222@gmail.com>
2023-03-14 23:51:58 +0300

    updated signin component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/visibilityIcon.svg'
 
 function SignIn() {
+    const navigate = useNavigate()
+    const [showPassword, setShowPassword] = useState(false)
     return (
         <div>
             <h1>Login</h1>
