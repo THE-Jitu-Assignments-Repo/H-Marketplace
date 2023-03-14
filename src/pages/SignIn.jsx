ad73621bdd2aa29c6d3145a87cdaf0896ffdbb14
mik284 <mikeykamau222@gmail.com>
2023-03-15 00:00:16 +0300

    added controlled form

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,27 +1,45 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
-import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
 function SignIn() {
-    const navigate = useNavigate()
-    const [showPassword, setShowPassword] = useState(false)
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-    })
-    return (
-        <>
-            <div className="pageContainer">
-                <header>
-                    <p className="pageHeader">Welcome Back!</p>
-                </header>
-                <form>
-                    <input type="email" className='eailInput' placeholder='Email' id='email' value={email} onChange={onChage} />
-                </form>
-            </div>
-        </>
-    )
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const onChange= ()=>{
+    
+  }
+  return (
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back!</p>
+        </header>
+        <form>
+          <input
+            type="email"
+            className="emailInput"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onChage}
+          />
+          <input
+            type={showPassword?'text' : 'password'}
+            className="passwordInput"
+            placeholder="Password"
+            id="password"
+            value={password}
+            onChange={onChage}
+          />
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default SignIn
+export default SignIn;
