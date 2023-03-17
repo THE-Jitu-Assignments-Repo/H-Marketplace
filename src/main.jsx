162adfe3840c01e4d43ee7394d4fd0c3108f5ef4
mik284 <mikeykamau222@gmail.com>
2023-03-17 23:58:44 +0300

    added react toastify

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {BrowserRouter} from "react-router-dom"
+import {ToastContaineer} from 'react-toastify'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ToastContaineer>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </ToastContaineer>
   </React.StrictMode>,
 )
