be2fd8d7e3a756b79d3d5ed6605aec6416a2f656
mik284 <mikeykamau222@gmail.com>
2023-04-02 23:19:44 +0300

    fixed profile view
    updated the private component

--- src/component/PrivateRoute.jsx
+++ src/component/PrivateRoute.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import {Navigate, Outlet} from 'react-router-dom'
+import useAuthStatus from '../Hooks/useAuthStatus';
 function PrivateRoute() {
-    const loggedIn = false
+    const [loggedIn, checkingStatus] = useAuthStatus()
+    if(checkingStatus){
+        return <h3>Loading....</h3>
+    }
     return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />;
 }
 export default PrivateRoute
