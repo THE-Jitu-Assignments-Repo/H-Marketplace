4d28a6435ea89420bf871f9fde757e29f7e146dc
mik284 <mikeykamau222@gmail.com>
2023-04-02 23:11:05 +0300

    modified routes

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -6,6 +6,7 @@ import Offers from "../pages/Offers";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import Profile from "../pages/Profile";
+import PrivateRoute from "../component/PrivateRoute";
 
 function Routers() {
   return (
@@ -14,7 +15,9 @@ function Routers() {
         {/* <Route index element={<Index />}/> */}
         <Route index path="/" element={<Explore />} />
         <Route path="/offers" element={<Offers />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/forgot-password" element={<ForgottPassword />} />
