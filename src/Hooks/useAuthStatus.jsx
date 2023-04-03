e74c60ec5629d2c94439a336ff021a5cab1b8618
mik284 <mikeykamau222@gmail.com>
2023-04-03 22:56:50 +0300

    Added a cleanup function

--- src/Hooks/useAuthStatus.jsx
+++ src/Hooks/useAuthStatus.jsx
@@ -2,20 +2,28 @@ import React from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { useRef } from 'react';
 function useAuthStatus() {
     const [loggedIn, setLoggedIn]= useState(false)
     const [checkingStatus, setCheckingStatus] = useState(true)
-
+    const isMounted = useRef(true)
 
     useEffect(()=>{
-        const auth = getAuth()
-        onAuthStateChanged(auth, (user)=>{
-            if(user){
-                setLoggedIn(true)
-            }
-            setCheckingStatus(false)
-        })
-    })
+        if(isMounted){
+
+            const auth = getAuth()
+            onAuthStateChanged(auth, (user)=>{
+                if(user){
+                    setLoggedIn(true)
+                }
+                setCheckingStatus(false)
+            })
+        }
+
+        return ()=>{
+            isMounted.current(false)
+        }
+    },[isMounted])
 
     return [loggedIn, checkingStatus]
 }
