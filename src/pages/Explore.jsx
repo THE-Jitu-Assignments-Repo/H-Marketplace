400e6c5674d96401ddd1b803c90a15bd81b54b00
mik284 <mikeykamau222@gmail.com>
2023-04-04 19:05:19 +0300

    updated explore rent and sale categories

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
-
+import React from "react";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import { Link } from "react-router-dom";
 function Explore() {
-    return (
-        <div>
-            <h1>Explore</h1>
+  return (
+    <div className="explore">
+      <header>
+        <p className="pageHeader">Explore</p>
+      </header>
+
+      <main>
+        {/* slides */}
+
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img
+              src={rentCategoryImage}
+              alt="rent"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for rent</p>
+          </Link>
+          <Link to="/category/sell">
+            <img
+              src={sellCategoryImage}
+              alt="rent"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for sale</p>
+          </Link>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
 
-export default Explore
+export default Explore;
