ec2cf09fd33cabc30e869c48761e41f145024a0e
mik284 <mikeykamau222@gmail.com>
2023-04-16 01:06:51 +0300

    done with creating listing page skeleton

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import Spinner from "../component/Spinner";
+import { useNavigate } from "react-router-dom";
 
 function CreateListing() {
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  const [loading, setLoading]= useState(false)
   const [formData, setFormData] = useState({
     type: "rent",
     name: "",
@@ -16,6 +21,31 @@ function CreateListing() {
     latitude: 0,
     longitude: 0,
   });
+
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setFormData({ ...formData, useRef: user.uid });
+        } else {
+          navigate("/sign-in");
+        }
+      });
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
+
+
+  if(loading){
+    return <Spinner />
+  }
   return <div>Create listing</div>;
 }
 
