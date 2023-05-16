d1c572b7f2db7f79f1ec1fd9c6d25f14a8c9bf4f
mik284 <mikeykamau222@gmail.com>
2023-05-16 19:58:21 +0300

    🐛 updated createlisting page

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -120,8 +120,32 @@ function CreateListing() {
         const storageRef = ref(storage, 'images/' + fileName)
         
         const uploadTask = uploadBytesResumable(storageRef, image)
-      })
+
+        uploadTask.on('state_changed', (snapshot) => {
+
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+         case 'paused':
+        console.log('Upload is paused');
+        break;
+        case 'running':
+        console.log('Upload is running');
+        break;
     }
+  }, 
+  (error) => {
+    reject(error)
+  }, 
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      console.log('File available at', downloadURL);
+    });
+  
+      })
+      
 
 
     setLoading(false)
